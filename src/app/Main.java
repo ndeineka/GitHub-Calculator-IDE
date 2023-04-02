24b26a1b12f0700ed35e37b0802bef7ae80cc60d
ndeineka <n.pleskanko@gmail.com>
2023-04-02 20:41:57 +0300

    Add app description

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,11 +1,12 @@
 package app;
 
 
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Super app!");
-
+        System.out.println("Calculator for base calculations");
         int a = 8;
         int b = 2;
         int c = add(a, b);
